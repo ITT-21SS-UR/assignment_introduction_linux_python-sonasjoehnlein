528bf3013e7da8d90a1b2347789b57093b1df966
Jonas <jonas.soehnlein@stud.uni-regensburg.de>
2021-04-19 17:05:01 +0200

    update count

--- count.sh
+++ count.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+ 
+# download the textfile
+wget ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/ibiblio/gnome/README
 
+cat README| tr '[A-Z]' '[a-z]'|      # make all text lowercase
+            tr ' ' "\n"|             # split in words per line
+            sort|                    # alphabetically list of words
+            uniq -c |                # without duplicates and count 
+            sort -nr|                # sort most common
+            head -11|                # first ten words
+            sed -E 's/^ *[0-9]+ //g' # without counts # https://superuser.com/questions/383726/get-the-most-common-appearing-lines-from-file-in-linux
