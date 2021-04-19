dc6165ae80df1cd212bda0ec6a4e05d3de916f08
Jonas <jonas.soehnlein@stud.uni-regensburg.de>
2021-04-19 18:31:21 +0200

    update stats

--- stats.py
+++ stats.py
@@ -1,16 +1,32 @@
 import sys
-import math  #import math module for math.sqrt
-import statistics  #import statistics module for median / standard deviation
+import math  
 
-# https://www.gutefrage.net/frage/wie-in-python-mehrere-zahlen-in-input-eingeben
+arr = [] #array or list for floats
+length = int(input("Please enter the range of numbers: ")) #Enter Range
 
-numbers_input = input("Geben Sie vier Zahlen ein (durch Komma getrennt): ")
-numbers_string = numbers_input.split(',')
-numbers = [float(numbers_string[i]) for i in range(len(numbers_string))]
-total_sum = sum(numbers)
+for i in range(length): #Fill the list with numbers
+    value = float(input("Enter the value: ")) #User input
+    arr.append(value)                       # put in the list
 
-print(total_sum)
+n = len(arr) #length for median
+arr.sort()   #sort the array for median
 
-print(math.sqrt(total_sum)) 
-print(statistics.stdev(numbers))  
-print(statistics.median(numbers))
+#Mean
+mean=sum(arr)/len(arr) #calculate the mean
+print("Mean is: " + str(mean))            # print out the mean
+
+
+#Median thread found in https://www.geeksforgeeks.org/finding-mean-median-mode-in-python-without-libraries/
+if n % 2 == 0:
+    median1 = arr[n//2]
+    median2 = arr[n//2 - 1]
+    median = (median1 + median2)/2
+else:
+    median = arr[n//2]
+print("Median is: " + str(median))
+
+
+#standard deviation Thread found in https://stackoverflow.com/questions/24023927/computing-standard-deviation-without-packages-in-python
+var  = sum(pow(x-mean,2) for x in arr) / len(arr)  # variance
+std  = math.sqrt(var)  # standard deviation
+print(std)
